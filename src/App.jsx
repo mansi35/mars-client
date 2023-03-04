bc6a5664a2916c8ecc1665551125777e9643eeb4
Shagun Yadav <shagun0915@gmail.com>
2023-03-04 19:05:37 +0530

    menu page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from './components/Header/Header';
 import Auth from './components/Auth/Auth';
 import Home from './pages/Home/Home';
 import CuisineSelector from './pages/CuisineSelector/CuisineSelector';
+import MenuPage from './pages/MenuPage/MenuPage';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
               <Route exact path="/auth" element={<Auth />} />
               <Route exact path="/" element={<Home />} />
               <Route exact path="/cuisines" element={<CuisineSelector />} />
+              <Route exact path="/menu" element={<MenuPage />} />
             </Routes>
           </div>
         </div>
---
+++ src/pages/MenuPage/MenuPage.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+// import Background from '../../components/Background/Background';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import MenuItem from '../../components/MenuItem/MenuItem';
+import { productData, responsive } from "../../components/MenuItem/data";
+import './MenuPage.scss';
+
+function MenuPage() {
+
+    const product = productData.map((item) => (
+        <MenuItem
+          name={item.name}
+          url={item.imageurl}
+          price={item.price}
+          description={item.description}
+        />
+      ));
+    
+  return (
+    <div className="Menupage">
+        {/* <Background /> */}
+        <div className='carousel'>
+            <h1>Menu</h1>
+            <Carousel showDots={true} responsive={responsive}>
+                {product}
+            </Carousel>
+        </div>
+
+    </div>
+  )
+}
+
+export default MenuPage
