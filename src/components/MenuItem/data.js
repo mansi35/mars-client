563ef84aecb2e6d28cf9a0620e87300d34c60729
Manasvi713 <manasvisingh713@gmail.com>
2023-03-04 22:58:06 +0530

    added images

--- src/components/MenuItem/data.js
+++ src/components/MenuItem/data.js
@@ -21,7 +21,7 @@ export const responsive = {
 
 export const productData = [{
         id: 1,
-        imageurl: "https://images.unsplash.com/photo-1560769629-975ec94e6a86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2R1Y3RzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        imageurl: "../../assets/Oxygen_vape.png",
         name: "Colorful sneakers",
         price: "$19.99",
         description: "Some text about the product..",
