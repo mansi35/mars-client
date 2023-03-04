451a607d460437a8012eda6cf00c98589e2f09fd
Deepanshi Singh <t-deesingh@microsoft.com>
2023-03-04 23:47:49 +0530

    cart-ui-2

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import CardCart from "../../components/CartCard/CardCart";
-import { useCart } from '../../context/CartContext'
 import "./styles.scss";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -12,23 +11,23 @@ const darkTheme = createTheme({
 });
 
 const CartPage = () => {
-  const {
-    cart,
-    showCartItems,
-    setShowCartItems,
-    showCart,
-    checkout
-  } = useCart()
-  const totalCents = cart.reduce((sum, entry) => {
-    return sum + entry.item.priceCents * entry.quantity
-  }, 0)
   return (
-    <div className='cartpage'>CartPage
-    <div className='cart-items'>
-        <CardCart/>
-    </div>
-    </div>
-  )
-}
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className="cartpage">
+        <div className="cart-items">
+          <CardCart />
+          <CardCart />
+          <CardCart />
+          <CardCart />
+          <CardCart />
+        </div>
+        <div className="payment-div">
+          <button className="pay-btn">Proceed to Pay</button>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+};
 
 export default CartPage;
