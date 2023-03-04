f62f4be35380e7e3a0940722a66ba3303e5c803a
Deepanshi Singh <t-deesingh@microsoft.com>
2023-03-04 18:06:50 +0530

    planets

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,73 @@
+import React from "react";
+import starsVideo from "../../assets/stars-19289.mp4";
+import "./Home.scss";
+
+function Home() {
+  return (
+    <div className="home">
+
+      <input
+        checked="checked"
+        class="planet2"
+        id="venus"
+        name="planet"
+        type="radio"
+      />
+      <label class="menu venus" for="venus">
+        <div class="preview"></div>
+        <div class="info">
+          <h2>
+            <div class="pip"></div>
+            Venus
+          </h2>
+          <h3>0.723 AU</h3>
+        </div>
+      </label>
+      <input
+        checked="checked"
+        class="planet1"
+        id="mercury"
+        name="planet"
+        type="radio"
+      />
+      <label class="menu mercury" for="mercury">
+        <div class="preview"></div>
+        <div class="info">
+          <h2>
+            <div class="pip"></div>
+            Mercury
+          </h2>
+          <h3>0.39 AU</h3>
+        </div>
+      </label>
+      <div class="solar">
+        <div class="solar_systm">
+          <div class="planet mercury">
+            <div class="planet_description mercury">
+              {/* <h2>Planet</h2> */}
+              <h1>Earth</h1>
+              <p>
+                Click on Mars to launch
+              </p>
+            </div>
+            <div class="overlay"></div>
+          </div>
+        </div>
+        <div class="solar_systm">
+          <div class="planet venus">
+            <div class="planet_description venus">
+              {/* <h2>Planet</h2> */}
+              <h1>Mars</h1>
+              <p>
+                Checkout out our dishes
+              </p>
+            </div>
+            <div class="overlay"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
