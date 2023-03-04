62ba8c2257ab2fc1a1dba5fcce4f7cd223efd412
Deepanshi Singh <t-deesingh@microsoft.com>
2023-03-04 23:37:07 +0530

    merge-conflict

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
-import CardCart from '../../components/CartCard/CardCart'
+import React from "react";
+import CardCart from "../../components/CartCard/CardCart";
 import { useCart } from '../../context/CartContext'
-import "./styles.scss"
+import "./styles.scss";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 const CartPage = () => {
   const {
@@ -16,12 +24,11 @@ const CartPage = () => {
   }, 0)
   return (
     <div className='cartpage'>CartPage
-      <div className='cart-items'>
-        <CardCart />
-      </div>
+    <div className='cart-items'>
+        <CardCart/>
     </div>
-
+    </div>
   )
 }
 
-export default CartPage
+export default CartPage;
