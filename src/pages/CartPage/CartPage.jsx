c69cb4f2da729162a4c6c59a6791c2935239c5e9
Aastha2602 <aasthaggarwal2000@gmail.com>
2023-03-05 09:33:16 +0530

    added payment gateway

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -8,6 +8,7 @@ import Select from "@mui/material/Select";
 import { useSelector } from 'react-redux'
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import GooglePayButton from '@google-pay/button-react';
 
 const darkTheme = createTheme({
   palette: {
@@ -60,7 +61,44 @@ const CartPage = () => {
 
         <div className="payment-div">
           <p>Total Price: ${price()}</p>
-          <button className="pay-btn">PROCEED TO PAY</button>
+          {/* <button className="pay-btn">PROCEED TO PAY</button> */}
+          <GooglePayButton
+            environment="TEST"
+            paymentRequest={{
+              apiVersion: 2,
+              apiVersionMinor: 0,
+              allowedPaymentMethods: [
+                {
+                  type: 'CARD',
+                  parameters: {
+                    allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+                    allowedCardNetworks: ['MASTERCARD', 'VISA'],
+                  },
+                  tokenizationSpecification: {
+                    type: 'PAYMENT_GATEWAY',
+                    parameters: {
+                      gateway: 'example',
+                      gatewayMerchantId: 'exampleGatewayMerchantId',
+                    },
+                  },
+                },
+              ],
+              merchantInfo: {
+                merchantId: '12345678901234567890',
+                merchantName: 'Demo Merchant',
+              },
+              transactionInfo: {
+                totalPriceStatus: 'FINAL',
+                totalPriceLabel: 'Total',
+                totalPrice: '100.00',
+                currencyCode: 'USD',
+                countryCode: 'US',
+              },
+            }}
+            onLoadPaymentData={paymentRequest => {
+              console.log('load payment data', paymentRequest);
+            }}
+          /> 
         </div>
       </div>
     </ThemeProvider>
